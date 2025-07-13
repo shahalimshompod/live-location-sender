0d9149165da3cc568facab981d5487fb2597c0e6
shahalimshompod <shahalimsompod@gmail.com>
2025-07-13 06:32:11 +0600

    added use client to signalR

--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -1,43 +1,17 @@
 "use client";
 import React, { useEffect } from "react";
-import dynamic from "next/dynamic";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
-// Dynamically import Leaflet components with SSR disabled
-const MapContainer = dynamic(
-  () => import("react-leaflet").then((mod) => mod.MapContainer),
-  { ssr: false }
-);
-const TileLayer = dynamic(
-  () => import("react-leaflet").then((mod) => mod.TileLayer),
-  { ssr: false }
-);
-const Marker = dynamic(
-  () => import("react-leaflet").then((mod) => mod.Marker),
-  { ssr: false }
-);
-const Popup = dynamic(() => import("react-leaflet").then((mod) => mod.Popup), {
-  ssr: false,
-});
-const useMap = dynamic(
-  () => import("react-leaflet").then((mod) => mod.useMap),
-  { ssr: false }
-);
-
 // Custom marker icon
-const icon = new L.Icon({
+const icon = L.icon({
   iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
-  iconRetinaUrl:
-    "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
-  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
   iconSize: [25, 41],
   iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
 });
 
-// Helper component to update map center
+// Helper component to update map center dynamically
 const RecenterMap = ({ lat, lon }) => {
   const map = useMap();
 
@@ -51,36 +25,24 @@ const RecenterMap = ({ lat, lon }) => {
 };
 
 const Map = ({ lat, lon }) => {
-  // Fix for default marker icons
+  // set custom marker icon globally (just once)
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      delete L.Icon.Default.prototype._getIconUrl;
-      L.Icon.Default.mergeOptions({
-        iconRetinaUrl: icon.options.iconRetinaUrl,
-        iconUrl: icon.options.iconUrl,
-        shadowUrl: icon.options.shadowUrl,
-      });
-    }
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({ iconUrl: icon.options.iconUrl });
   }, []);
-
-  if (typeof window === "undefined") {
-    return <div className="h-[500px] w-full bg-gray-200">Loading map...</div>;
-  }
 
   return (
     <MapContainer
       center={[lat, lon]}
       zoom={15}
       style={{ height: "500px", width: "100%" }}
-      scrollWheelZoom={false}
     >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <Marker position={[lat, lon]} icon={icon}>
         <Popup>Live Location</Popup>
       </Marker>
+
+      {/* 🔁 Keep map centered on new location updates */}
       <RecenterMap lat={lat} lon={lon} />
     </MapContainer>
   );
